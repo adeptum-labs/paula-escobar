a6502b89d803491fa3908b550065a640b2b31bd7
Adam Waldenberg <adam@adeptum.se>
2026-09-17 08:24:29 +0200

    Offer Pro Tracker 2 modules to the player

--- src/test/java/com/adeptum/paula/module/ModuleLoaderRegistryTest.java
+++ src/test/java/com/adeptum/paula/module/ModuleLoaderRegistryTest.java
@@ -27,6 +27,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.adeptum.paula.module.ape.ApeLoader;
 import com.adeptum.paula.module.ay.AyLoader;
+import com.adeptum.paula.module.ay.ProTracker2Loader;
 import com.adeptum.paula.module.ay.ProTrackerLoader;
 import com.adeptum.paula.module.ay.SoundTrackerLoader;
 import com.adeptum.paula.module.composer669.Composer669Loader;
@@ -56,7 +57,8 @@ class ModuleLoaderRegistryTest {
     void listsBuiltInFormats() {
         assertEquals(List.of(JavaModLoader.FORMAT, SidLoader.FORMAT, SapLoader.FORMAT, DigiBoosterLoader.FORMAT,
                 HivelyLoader.FORMAT, MedLoader.FORMAT, Composer669Loader.FORMAT, XTrackerLoader.FORMAT,
-                UltLoader.FORMAT, AyLoader.FORMAT, SoundTrackerLoader.FORMAT, ProTrackerLoader.FORMAT, Mo3Loader.FORMAT, Mp3Loader.FORMAT,
+                UltLoader.FORMAT, AyLoader.FORMAT, SoundTrackerLoader.FORMAT, ProTracker2Loader.FORMAT,
+                ProTrackerLoader.FORMAT, Mo3Loader.FORMAT, Mp3Loader.FORMAT,
                 FlacLoader.FORMAT, WavLoader.FORMAT, OggLoader.FORMAT, ApeLoader.FORMAT),
                 registry.formats());
     }
@@ -75,6 +77,7 @@ class ModuleLoaderRegistryTest {
         assertTrue(registry.loaderFor(Path.of("x.psg")).isPresent());
         assertTrue(registry.loaderFor(Path.of("x.ym")).isPresent());
         assertTrue(registry.loaderFor(Path.of("x.stc")).isPresent());
+        assertTrue(registry.loaderFor(Path.of("x.pt2")).isPresent());
         assertTrue(registry.loaderFor(Path.of("x.pt3")).isPresent());
         assertTrue(registry.loaderFor(Path.of("x.mmd1")).isPresent());
         assertTrue(registry.loaderFor(Path.of("x.mp3")).isPresent());
